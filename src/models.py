7884f5f0ee8d22fe5827bafcf23802ef5616d1f0
Ian Fiddes <ifiddes@npk.kilokluster.ucsc.edu>
2015-02-04 09:52:48 -0800

    almost done

--- src/models.py
+++ src/models.py
@@ -34,6 +34,8 @@ class ModelWrapper(Target):
         self.dataPenalty = dataPenalty
         self.graph = graph
         self.key = open(keyFile).readline().rstrip()
+        #index is a bwa index of the region to be aligned to (one copy of notch2)
+        self.index = "./data/SUN_data/hs_n2.masked.fa"
 
     def downloadQuery(self):
         """
@@ -44,65 +46,67 @@ class ModelWrapper(Target):
         system("""curl --silent "{}" -u "{}" | samtools bamshuf -Ou /dev/stdin {} """
                 """| samtools bam2fq /dev/stdin > {}""".format(self.queryString, 
                 "haussler:" + self.key, os.path.join(self.getLocalTempDir(), "tmp"), fastqFile))
-        return fastqFile
-
-    def combinedPlot(self, ilpDict, sunDict, maxPos, offset, normalizing):
-        arrays = {}
-        colors = ["#4D4D4D","#5DA5DA","#FAA43A","#60BD68","#F17CB0"]
-        xvals = np.array(range(offset, offset + maxPos + 1),dtype="int")
-        sortedParalogs = sorted(ilpDict.keys())
-        for para in sortedParalogs:
-            arrays[para] = np.array(ilpDict[para], dtype="int")
-        ax = plt.gca()
-        fig, plots = plt.subplots(len(sortedParalogs), sharex=True, sharey=True)
-        plt.yticks((0, 1, 2, 3, 4))
-        for i, (p, para) in enumerate(izip(plots, sortedParalogs)):
-            p.axis([offset, offset + maxPos + 1, 0, 4])
-            p.fill_between(xvals, ilpDict[para], color=colors[i], alpha=0.7)
-            p.set_title("{} Copy Number".format(para))
-            sunPos, sunVals = zip(*sunDict[para])
-            plt.vlines(np.asarray(sunPos), np.zeros(len(sunPos)), np.asarray(sunVals), lw=2)
-        fig.subplots_adjust(hspace=0.5)
-        plt.setp([a.get_xticklabels() for a in fig.axes[:-1]], visible=False) 
-        plt.savefig(os.path.join(self.baseOutDir, self.uuid, self.uuid[:8] + ".sun.ilp.png"), format="png")
-        plt.close()     
-
-    def run(self):
-        fastqFile = self.downloadQuery()
         if os.path.getsize(fastqFile) < 513:
             raise RuntimeError("curl did not download a BAM for {}. exiting.".format(self.uuid))
-        sun = FilteredSunModel(self.baseOutDir, fastqFile, self.uuid, self.getLocalTempDir())
-        sun.run()
-        unfilteredSun = UnfilteredSunModel(self.baseOutDir, fastqFile, self.uuid, self.getLocalTempDir())
-        unfilteredSun.run()
-        ilp = IlpModel(self.baseOutDir, self.bpPenalty, self.dataPenalty, fastqFile, self.uuid, 
-                self.graph, self.getLocalTempDir())
-        ilp.run()
-        self.combinedPlot(ilp.resultDict, sun.resultDict, ilp.maxPos, ilp.offset, sun.normalizing)
-
-
-class SunModel(object):
-    """
-    Runs the SUN model, where alelle fraction at unique sites is used to infer copy number
-    """
-    def filterAndIndex(self, bamIn, bamOut):
-        """
-        Filter for unmapped reads and change position to be absolute to the chromosome
-        """
+        #align the extracted reads to the index
+        sortedBamPath = os.path.join(self.getLocalTempDir(), "{}.sorted".format(self.uuid))
+        system("bwa mem -v 1 {} {} | samtools view -bS - | samtools sort - {}".format(self.index, fastqFile, sortedBamPath))
+        #samtools appends .bam to sorted bam files
+        sortedBamPath += ".bam"
+        #filter the SAM records and find the site coverage at each locus, creating VCFs
+        remappedBamPath = os.path.join(self.getLocalTempDir(), "{}.remapped.sorted.bam".format(self.uuid))
         header = {"HD": {"VN": "1.3"}, "SQ": [{"LN": 248956422, "SN": "chr1"}]}
-
-        outfile = pysam.Samfile(bamOut, "wb", header=header)
-        bamfile = pysam.Samfile(bamIn, "rb")  
-
+        outfile = pysam.Samfile(remappedBamPath, "wb", header=header)
+        bamfile = pysam.Samfile(sortedBamPath, "rb")  
         for record in bamfile:
             if not record.is_unmapped:
                 chrom, span = bamfile.getrname(record.tid).split(":")
                 start, end = map(int, span.split("-"))
                 record.pos = record.pos + start - 1
                 outfile.write(record)
-
         outfile.close()
-        system("samtools index {}".format(bamOut))
+        system("samtools index {}".format(remappedBamPath))
+        return remappedBamPath, fastqFile 
+
+    def combinedPlot(self, ilpDict, filteredSunDict, unfilteredSunDict, maxPos, offset):
+        colors = ["#4D4D4D", "#5DA5DA", "#FAA43A", "#60BD68"]
+        #used because the ILP model uses single letter labels
+        paraMap = {"Notch2NL-A":"A", "Notch2NL-B":"B", "Notch2NL-C":"C", "Notch2NL-D":"D"}
+        xvals = np.array(range(offset, offset + maxPos + 1), dtype="int")
+        sortedParalogs = ["Notch2NL-A", "Notch2NL-B", "Notch2NL-C", "Notch2NL-D"]
+        ax = plt.gca()
+        fig, plots = plt.subplots(len(sortedParalogs), sharex=True, sharey=True)
+        plt.yticks((0, 1, 2, 3, 4))
+        plt.suptitle("kmer-DeBruijn ILP and SUN results")
+        for i, (p, para) in enumerate(izip(plots, sortedParalogs)):
+            p.axis([offset, offset + maxPos + 1, 0, 4])
+            p.fill_between(xvals, ilpDict[para], color=colors[i], alpha=0.7)
+            p.set_title("{}".format(para))
+            sunPos, sunVals = zip(*unfilteredSunDict[paraMap[para]])
+            p.vlines(np.asarray(sunPos), np.zeros(len(sunPos)), sunVals, color="#F17CB0")
+            sunPos, sunVals = zip(*filteredSunDict[paraMap[para]])
+            p.vlines(np.asarray(sunPos), np.zeros(len(sunPos)), sunVals, color="#763D56")            
+        fig.subplots_adjust(hspace=0.5)
+        plt.setp([a.get_xticklabels() for a in fig.axes[:-1]], visible=False) 
+        plt.savefig(os.path.join(self.baseOutDir, self.uuid, self.uuid[:8] + ".png"), format="png")
+        plt.close()     
+
+    def run(self):
+        bamPath, fastqFile = self.downloadQuery()
+        sun = FilteredSunModel(self.baseOutDir, self.uuid, bamPath)
+        sun.run()
+        unfilteredSun = UnfilteredSunModel(self.baseOutDir, self.uuid, bamPath)
+        unfilteredSun.run()
+        ilp = IlpModel(self.baseOutDir, self.bpPenalty, self.dataPenalty, fastqFile, self.uuid, 
+                self.graph, self.getLocalTempDir())
+        ilp.run()
+        self.combinedPlot(ilp.resultDict, sun.resultDict, unfilteredSun.resultDict, ilp.maxPos, ilp.offset)
+
+
+class SunModel(object):
+    """
+    Runs the SUN model, where alelle fraction at unique sites is used to infer copy number
+    """
 
     def findSiteCoverages(self, bamIn):
         """
@@ -110,42 +114,46 @@ class SunModel(object):
         """
         bases = set(["A", "T", "G", "C", "a", "t", "g", "c"])
         resultDict = defaultdict(list)
-        
+        nVals = []
         for pos, (para, ref, alt) in self.wl.iteritems():
             posStr = "chr1:{0}-{0}".format(pos)
             pileUp = pysam.mpileup("-q", "10", "-r", posStr, bamIn)
             if len(pileUp) == 0:
                 continue
-
             pileUpStr = pileUp[0].split()
             if len(pileUpStr) != 6:
                 continue
-
             pileUpResult = Counter(x.upper() for x in pileUpStr[4] if x in bases)
             if ref not in pileUpResult or alt not in pileUpResult:
                 continue
-
             frac = formatRatio(pileUpResult[alt], sum(pileUpResult.values()))
             #invert fraction for Notch2 paralogs
             if para == "N":
                 frac = 1 - frac
-            resultDict[para].append([pos, frac])
+                nVals.append(frac)
+            else:
+                resultDict[para].append([pos, frac])
+        norm = self.findNormalizingFactor(nVals)
+        for para, result in resultDict.iteritems():
+            resultDict[para] = [[x, 2.0 * y / norm] for x,y in result]
         return resultDict
 
     def findNormalizingFactor(self, r):
         """
         The values for Notch2NL-A-D are normalized by Notch2. Notch2 does not have CNV.
         """
-        return np.mean(rejectOutliers(np.asarray(r))) / 0.2
+        return np.mean(rejectOutliers(np.asarray(r)))
 
-    def plotHistograms(self, resultDict, path):
-        f, plts = plt.subplots(5, sharex=True)
+    def plotHistograms(self, resultDict):
+        path = os.path.join(self.outDir, "{}.{}.png".format(self.uuid, self.__class__.__name__))
+        f, plts = plt.subplots(4, sharex=True)
         for para, p in izip(sorted(resultDict.keys()), plts):
-            v = np.asarray(zip(*resultDict[para])[1]) * self.normalizing
-            p.set_ylabel("{}".format(para), size=9)
-            p.hist(v, bins=30, range=(0.0,1.0), color='#1e90ff', normed=True)
+            p.set_title("Notch2NL-{}".format(para))
+            p.hist(np.asarray(zip(*resultDict[para])[1]), bins=40, range=(0.0,4.0), color='#1e90ff', normed=True)
             p.yaxis.tick_right()
-            setp(p.get_yticklabels(), fontsize=8)
+            setp(p.get_yticklabels(), fontsize=9)
+        plt.xticks((0, 1, 2, 3, 4))
+        f.subplots_adjust(hspace=0.4)
         plt.savefig(path)
 
     def makeBedgraphs(self, resultDict):
@@ -153,54 +161,31 @@ class SunModel(object):
             os.mkdir(os.path.join(self.outDir, "bedgraphs"))
         for para in resultDict:
             path = os.path.join(self.outDir, "bedgraphs", "{}.Notch2NL-{}.{}.bedGraph".format( 
-                    self.uuid[:8], para, self.header))
+                    self.uuid[:8], para, self.__class__.__name__))
             bedHeader = ("track type=bedGraph name={} autoScale=off visibility=full alwaysZero=on "
                     "yLineMark=0.2 viewLimits=0.0:0.4 yLineOnOff=on maxHeightPixels=100:75:50\n")
             with open(path, "w") as outf:
                 outf.write(bedHeader.format(self.uuid[:8] + "_" + para))
                 for pos, frac in resultDict[para]:
-                    outf.write("\t".join(map(str, ["chr1", pos, pos + 1, frac * self.normalizing])) + "\n")
+                    outf.write("\t".join(map(str, ["chr1", pos, pos + 1, frac])) + "\n")
 
     def run(self):
-        #align the extracted reads to the index
-        sortedBamPath = os.path.join(self.localTempDir, "{}.{}.sorted".format(self.uuid, self.header))
-        system("bwa mem -v 1 {} {} | samtools view -bS - | samtools sort - {}".format(self.index, 
-                self.fastqFile, sortedBamPath))
-        #samtools appends .bam to sorted bam files
-        sortedBamPath += ".bam"
-        #filter the SAM records and find the site coverage at each locus, creating VCFs
-        remappedBamPath = os.path.join(self.localTempDir, 
-                "{}.{}.remapped.sorted.bam".format(self.uuid, self.header))
-        self.filterAndIndex(sortedBamPath, remappedBamPath)
-        self.resultDict = self.findSiteCoverages(remappedBamPath)     
-        self.normalizing = self.findNormalizingFactor(zip(*self.resultDict["N"])[1])
+        self.resultDict = self.findSiteCoverages(self.bamPath)
         #plot the results
-        self.plotHistograms(self.resultDict, os.path.join(self.outDir, "{}.{}.png".format(self.uuid, 
-                self.header)))
+        self.plotHistograms(self.resultDict)
         self.makeBedgraphs(self.resultDict)
         #need to add (SUN-based) ILP here - hasn't been working with WGS data
         #self.call_ilp()
-        pickle.dump(self.resultDict, open(os.path.join(self.outDir, "resultDict.pickle"), "w"))
+        #pickle.dump(self.resultDict, open(os.path.join(self.outDir, "resultDict.pickle"), "w"))
 
 
 class UnfilteredSunModel(SunModel):
-    def __init__(self, baseOutDir, fastqFile, uuid, localTempDir):
+    def __init__(self, baseOutDir, uuid, bamPath):
         self.uuid = uuid
-        self.fastqFile = fastqFile
-        self.localTempDir = localTempDir
-
+        self.bamPath = bamPath
         self.outDir = os.path.join(baseOutDir, self.uuid)
         if not os.path.exists(self.outDir):
             os.mkdir(self.outDir)
-
-        ###########
-        # these are hard coded files. Shitty but whatever.
-        ###########
-
-        self.header = "unfilteredSun"
-        #index is a bwa index of the region to be aligned to (one copy of notch2)
-        self.index = "./data/SUN_data/hs_n2.masked.fa"
-
         #whitelist is a text file of whitelisted SUN positions - in this case, unfiltered
         with open("./data/SUN_data/unfiltered_whitelist.txt") as wl:
             wl_list = [x.split() for x in wl if not x.startswith("#")]
@@ -213,23 +198,12 @@ class UnfilteredSunModel(SunModel):
 
 
 class FilteredSunModel(SunModel):
-    def __init__(self, baseOutDir, fastqFile, uuid, localTempDir):
+    def __init__(self, baseOutDir, uuid, bamPath):
         self.uuid = uuid
-        self.fastqFile = fastqFile
-        self.localTempDir = localTempDir
-
+        self.bamPath = bamPath
         self.outDir = os.path.join(baseOutDir, self.uuid)
         if not os.path.exists(self.outDir):
             os.mkdir(self.outDir)
-
-        ###########
-        # these are hard coded files. Shitty but whatever.
-        ###########
-
-        self.header = "filteredSun"
-        #index is a bwa index of the region to be aligned to (one copy of notch2)
-        self.index = "./data/SUN_data/hs_n2.masked.fa"
-
         #whitelist is a text file of whitelisted SUN positions - in this case, unfiltered
         with open("./data/SUN_data/whitelist.txt") as wl:
             wl_list = [x.split() for x in wl if not x.startswith("#")]
@@ -275,10 +249,11 @@ class IlpModel(object):
         plt.legend(patches, sortedParalogs)
         plt.suptitle("kmer-DeBruijn ILP results Notch2NL")
         plt.ylabel("Inferred Copy Number")
-        plt.savefig(os.path.join(self.outDir, self.uuid[:8] + ".png"), format="png")
+        plt.savefig(os.path.join(self.outDir, self.uuid[:8] + "combined.ILP.png"), format="png")
         plt.close()
         #now do individual plots on one png
         fig, plots = plt.subplots(len(sortedParalogs), sharex=True, sharey=True)
+        plt.suptitle("kmer-DeBruijn ILP results Notch2NL")
         plt.yticks((0, 1, 2, 3, 4))
         for i, (p, para) in enumerate(izip(plots, sortedParalogs)):
             p.axis([offset, offset + maxPos + 1, 0, 4])
@@ -287,7 +262,7 @@ class IlpModel(object):
             p.scatter(xvals, copyMap[para], color=colors[i], alpha=0.7, s=0.3)
         fig.subplots_adjust(hspace=0.5)
         plt.setp([a.get_xticklabels() for a in fig.axes[:-1]], visible=False)
-        plt.savefig(os.path.join(self.outDir, self.uuid[:8] + ".separated.png"), format="png")
+        plt.savefig(os.path.join(self.outDir, self.uuid[:8] + ".separated.ILP.png"), format="png")
         plt.close()
 
     def run(self):

150eebc4028325a7e9d944aff3431385f3baa683
ifiddes <ian.t.fiddes@gmail.com>
2015-02-11 09:52:21 -0800

    .

--- src/downloadedPipeline.py
+++ src/downloadedPipeline.py
@@ -14,12 +14,12 @@ def buildParser():
     parser.add_argument("--output", "-o", type=DirType, action=FullPaths, default="./output/",
             help=("base output directory that results will be written to. Default is ./output/"
             "For this model is where files will be hunted for."))
-    parser.add_argument("--breakpoint_penalty", type=float, default=25.0,
+    parser.add_argument("--breakpoint_penalty", type=float, default=20.0,
             help="breakpoint penalty used for ILP model.")
-    parser.add_argument("--data_penalty", type=float, default=0.65,
+    parser.add_argument("--data_penalty", type=float, default=5,
             help="data penalty used for ILP model.")
     parser.add_argument("--graph", type=str, action=FullPaths,
-            default="./data/graphs/Notch2NL.pickle")
+            default="./data/graphs/HugeGraph.pickle")
     parser.add_argument("--save_intermediate", action="store_true",
             help="Should we store the intermediates for debugging?")
     return parser
